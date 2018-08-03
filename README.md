a3ad48387e7daf33b472615991b043acb29fd6b0
sleeplesseditor <josh@bryson.me.uk>
2018-08-03 15:15:44 +0100

    Router setup

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 An app that presents an alternative to the Giphy site built using React and Cloudinary, and using Chilezie Unachukwu's [tutorial](https://scotch.io/tutorials/build-your-own-giphy-alternative-with-react).
 
+You will need to create an account with [Cloudinary](https://cloudinary.com), and then create a config/keys.js files for the cloudName and uploadPreset keys for Nav.js. To get this project up and running, follow these steps: 
+
+1) cd GiphyAlternative
+2) npm install
+3) npm start
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Dashboard from './components/Dashboard';
+import Create from './components/Create';
+
+const App = () => {
+      console.log('App component is running');
+      return (
+          <BrowserRouter>
+            <div className="container">
+              <Route path="/" component={Dashboard} />
+              <Route path="/create" component={Create} />
+            </div>
+          </BrowserRouter>
+      );
 }
 
 export default App;
